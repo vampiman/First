06541235966ac78d7639e72236f0db5476f735aa
vampiman <robert.leu2009@yahoo.com>
2018-08-30 10:58:29 +0300

    Basic algorithm

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 var http = require('http');
 var server = require('express');
 
@@ -5,3 +6,18 @@ http.createServer((req,res) => {
     res.write("Hello mate");
     res.end();
 }).listen(8080,console.log('Listening to port 8080'));
+*/
+
+var rectangle = {
+    area: (x,y)=> (x*y),
+
+    perimeter: (x,y) => ((x+y)*2)
+
+};
+
+function solveRect(l,L)
+{
+    console.log(`Area is ${rectangle.area(l,L)} and perimeter is ${rectangle.perimeter(l,L)}`);
+}
+
+solveRect(2,2);
